7ee1da629adfecde981eb8554a6aae479371650f
mr <xgit@redmond5.com>
2015-02-21 17:01:43 -0500

    Initial Commit

---
+++ CoinbaseStatusBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CoinbaseStatusBar
+//
+//  Created by mr on 2/21/15.
+//  Copyright (c) 2015 mr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
