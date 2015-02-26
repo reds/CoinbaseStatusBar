02e1e8ea5d91848b9c7b0730872b4b91980fdd83
mr <xgit@redmond5.com>
2015-02-25 20:02:14 -0500

    Reconnect after server/network error.

--- CoinbaseStatusBar/AppDelegate.swift
+++ CoinbaseStatusBar/AppDelegate.swift
@@ -2,47 +2,31 @@
 //  AppDelegate.swift
 //  CoinbaseStatusBar
 //
-//  Created by mr on 2/21/15.
-//  Copyright (c) 2015 mr. All rights reserved.
+//  Created by redmond.martin@gmail.com on 2/21/15.
+//  Copyright (c) 2015 redmond.martin@gmail.com All rights reserved.
 //
 
 import Foundation
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSStreamDelegate, NSApplicationDelegate {
+    
+    let serverHost:String = "chat52.com"
+    let serverPort:Int = 5898
     
     @IBOutlet weak var window: NSWindow!
     
     var statusBar = NSStatusBar.systemStatusBar()
     var statusBarItem : NSStatusItem = NSStatusItem()
-    var menu: NSMenu = NSMenu()
-    var menuItem : NSMenuItem = NSMenuItem()
     
     override func awakeFromNib() {
-        println ( "awake")
-        // theLabel.stringValue = "You've pressed the button \n \(buttonPresses) times!"
-        
-        //Add statusBarItem
         statusBarItem = statusBar.statusItemWithLength(-1)
-        // statusBarItem.menu = menu
-        //        statusBarItem.title = "Presses"
-        
-        //Add menuItem to menu
-        menuItem.title = "Clicked"
-        menuItem.action = Selector("setWindowVisible:")
-        menuItem.keyEquivalent = ""
-        // menu.addItem(menuItem)
     }
-    
-    //    func applicationDidFinishLaunching(aNotification: NSNotification?) {
-    //        statusBarItem.title = "adfl"
-    //        self.window!.orderOut(self)
-    //    }
     
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         self.window!.orderOut(self)
-        connect2server ( "chat52.com", port: 5898 )
+        connect2server ( serverHost, port: serverPort )
         send("login")
     }
     
@@ -51,52 +35,80 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             self.statusBarItem.title = msg
         }
     }
-
-    let netQueue = dispatch_queue_create("tcp-client-queue", DISPATCH_QUEUE_SERIAL)
     
-    func connect2server ( host: String, port: Int ) {
-        var inputStream:NSInputStream?
-        var outputStream:NSOutputStream?
-        NSStream.getStreamsToHostWithName(host, port: port, inputStream: &inputStream, outputStream: &outputStream)
-        
-        if inputStream == nil || outputStream == nil {
-            doDisplay("Error connecting")
-            return
+    let bufferSize = 1024
+    var buffer = Array<UInt8>(count: 1024, repeatedValue: 0)
+    func handleRead(s: NSInputStream) {
+        let bytesRead = s.read(&buffer, maxLength: bufferSize)
+        if bytesRead > 0 {
+            var output:String = NSString(bytes: &buffer, length: bytesRead, encoding: NSUTF8StringEncoding)!
+            var lines = split ( output) {$0 == "\n"}
+            var vals = split ( lines.last! ) {$0 == " "}
+            self.doDisplay(vals.first!)
         }
-        
-        var input = inputStream!
-        dispatch_async(netQueue) {
-            input.open()
-            let bufferSize = 1024
-            var buffer = Array<UInt8>(count: bufferSize, repeatedValue: 0)
-            while (true) {
-                let bytesRead = input.read(&buffer, maxLength: bufferSize)
-                if bytesRead >= 0 {
-                    var output:String = NSString(bytes: &buffer, length: bytesRead, encoding: NSUTF8StringEncoding)!
-                    var lines = split ( output) {$0 == "\n"}
-                    var vals = split ( lines.last! ) {$0 == " "}
-                    self.doDisplay(vals.first!)
-                    println ( vals )
-                } else {
-                    self.doDisplay ( "Connection Lost")
+    }
+    
+    var inputStream:NSInputStream?
+    var outputStream:NSOutputStream?
+
+    func stream(s: NSStream, handleEvent eventCode: NSStreamEvent) {
+        switch eventCode {
+        case NSStreamEvent.HasBytesAvailable:
+            if let i = inputStream {
+                handleRead(i)
+            }
+        case NSStreamEvent.ErrorOccurred:
+            doDisplay ( "Network Error")
+            netCloseAndReconnect()
+        case NSStreamEvent.EndEncountered:
+            doDisplay ("Server Error")
+            netCloseAndReconnect()
+        default:
+            break;
+        }
+    }
+    
+    func send(msg:String) {
+        if let o = outputStream {
+            if o.hasSpaceAvailable {
+                dispatch_async(dispatch_get_main_queue()) {
+                    o.write(msg, maxLength: countElements(msg))
                     return
                 }
             }
         }
-        output = outputStream!
-        output?.open()
+    }
+    
+    func connect2server ( host: String, port: Int ) {
+        NSStream.getStreamsToHostWithName(host, port: port, inputStream: &inputStream, outputStream: &outputStream)
+        netOpen()
         self.doDisplay("Connecting")
     }
 
-    var output: NSOutputStream?
+    func netOpen () {
+        inputStream?.delegate = self
+        outputStream?.delegate = self
+        inputStream?.scheduleInRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+        outputStream?.scheduleInRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+        inputStream?.open()
+        outputStream?.open()
+    }
 
-    func send (msg: String) {
-        dispatch_async(netQueue) {
-            let data = msg.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
-            if let out = self.output {
-                out.write (UnsafePointer(data.bytes), maxLength:data.length )
-            }
-            println ( "sending ", msg )
+    var reconnecting = false
+    func netCloseAndReconnect () {
+        inputStream?.close()
+        outputStream?.close()
+        inputStream?.removeFromRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+        outputStream?.removeFromRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+        if reconnecting {
+            return
+        }
+        reconnecting = true
+        let delta = NSEC_PER_SEC * 2
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delta) ),
+            dispatch_get_main_queue()) {
+                self.connect2server(self.serverHost, port: self.serverPort)
+                self.reconnecting = false
         }
     }
     
